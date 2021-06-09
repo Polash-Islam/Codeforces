ad9abf07a1abccadba56500026723e8614933bbc
Polash-Islam <85650007+Polash-Islam@users.noreply.github.com>
2021-06-10 04:11:01 +0600

    Add files via upload

---
+++ A. Bear and Big Brother.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main ()
+{  int a,b;
+int p=0;
+     cin>>a>>b;
+   while (a<=b){
+    a=a*3;
+    b=b*2;
+    p++;
+   }
+   cout<<p<<endl;
+
+
+
+    return 0;
+}
